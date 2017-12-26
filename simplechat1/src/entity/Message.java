1fad829dfc1b2cdf3fe273b5fe1be6e5b4e83810
Itai Tal <Itaital100@gmail.com>
2017-12-26 16:49:14 +0200

    I forget to add message class I add it now

---
+++ simplechat1/src/entity/Message.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Message implements Serializable{
+	
+	Object msg;
+	String option;
+	
+	public Message(Object msg,String option) {
+		
+		this.msg = msg;
+		this.option = option;
+	}
+
+	public Object getMsg() {
+		return msg;
+	}
+
+	public void setMsg(Object msg) {
+		this.msg = msg;
+	}
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+	
+
+	
+
+}
