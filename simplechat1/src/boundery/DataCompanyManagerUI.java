02766d26e8c0e774e048c6c3cb58daed8534df49
Ariel Yaakov <ArielYaakov3081@gmail.com>
2017-12-31 23:16:00 +0200

    DataCompanyManagerUI.java Din's Class
    Signed-off-by: Ariel Yaakov <ArielYaakov3081@gmail.com>

---
+++ simplechat1/src/boundery/DataCompanyManagerUI.java
@@ -0,0 +1,33 @@
+package boundery;
+
+import java.util.Scanner;
+import java.util.Vector;
+import controller.DataCompanyManagerController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import mypackage.ClientConsole;
+import entity.User;
+
+public class DataCompanyManagerUI extends Application /* With This Class We Show the Product GUI */{
+	
+	public static Vector<User> users = new Vector<User>();
+	public static ClientConsole myClient;
+	
+	public static void main( String args[] ) throws Exception
+	{ 
+		System.out.println("Please enter the server IP");
+		Scanner scanner = new Scanner(System.in);
+		String IP = scanner.next(); /* Enter Server IP */
+		myClient = new ClientConsole(IP, 5555);
+        launch(args);		
+	} 
+	
+	@Override
+	public void start(Stage arg0) throws Exception 
+	{		
+		DataCompanyManagerController aFrame = new DataCompanyManagerController(); /* Create CatalogFrame */				  
+		aFrame.start(arg0);
+	}
+
+}
+
