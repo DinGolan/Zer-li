02db296b21ab130983629798d5011a42937f9e89
Din Golan <dingolan100@gmail.com>
2017-12-31 22:57:48 +0200

    Fix

--- simplechat1/src/mypackage/ClientConsole.java
+++ simplechat1/src/mypackage/ClientConsole.java
@@ -7,6 +7,7 @@ package mypackage;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import boundery.DataCompanyManagerUI;
 import boundery.ProductUI;
 import boundery.UserUI;
 import client.ChatClient;
