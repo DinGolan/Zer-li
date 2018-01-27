2cd237ea8a730877ad61f8e2e134b1af055a6617
Ariel Yaakov <ArielYaakov3081@gmail.com>
2018-01-27 23:17:28 +0200

    תיעוד
    Signed-off-by: Ariel Yaakov <ArielYaakov3081@gmail.com>

--- simplechat1/src/boundery/SurveyUI.java
+++ simplechat1/src/boundery/SurveyUI.java
@@ -5,6 +5,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import mypackage.ClientConsole;
 
+/**
+ * class to save static variables that belong to the Survey
+ */
 public class SurveyUI extends Application {
 	public static ClientConsole myClient;
 	
