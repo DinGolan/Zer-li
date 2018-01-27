2b1079a45ecd226464ddc0534fdb731c6b13a661
may steinfeld <mayste17@hotmail.com>
2018-01-27 23:24:58 +0200

    finishhhhhhhhhhhhhhhhhhhhh
    Signed-off-by: may steinfeld <mayste17@hotmail.com>

--- simplechat1/src/entity/Message.java
+++ simplechat1/src/entity/Message.java
@@ -3,29 +3,55 @@ package entity;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
+
+/**
+ * class to store a message
+ * 1- the option by a string- what the query is
+ * 2- the object that we need for the query
+ */
 public class Message implements Serializable{
 	
 	Object msg;
 	String option;
-	
+
+	/**
+	 * constructor
+	 * @param msg- object
+	 * @param option- String
+	 */
 	public Message(Object msg,String option) {
 		
 		this.msg = msg;
 		this.option = option;
 	}
 
+	/**
+	 * getter for the object
+	 */
 	public Object getMsg() {
 		return msg;
 	}
 
+	/**
+	 * setter for the object msg
+	 * @param msg
+	 */
 	public void setMsg(Object msg) {
 		this.msg = msg;
 	}
 
+	/**
+	 * getter for the option
+	 * @return String- the option
+	 */
 	public String getOption() {
 		return option;
 	}
 
+	/**
+	 * setter for the option
+	 * @param option- String
+	 */
 	public void setOption(String option) {
 		this.option = option;
 	}
--- simplechat1/src/entity/Store.java
+++ simplechat1/src/entity/Store.java
@@ -2,14 +2,26 @@ package entity;
 
 import java.io.Serializable;
 
+/**
+ *class for the store entity- variables: store id, address, amount of orders and constructor
+ */
 public class Store implements Serializable {
 	
 	private int StoreId;
 	private String Store_Address;
 	private int QuantityOfOrders;
 	
-	public Store() {}
-	
+	/**
+	 * empty constructor
+	 */
+	public Store() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * auto filled constructor
+	 */
 	public Store(int storeId, String store_Address, int quantityOfOrders) {
 		super();
 		StoreId = storeId;
@@ -17,26 +29,56 @@ public class Store implements Serializable {
 		QuantityOfOrders = quantityOfOrders;
 	}
 	
+	/**
+	 * store id getter
+	 * @return int- store number
+	 */
 	public int getStoreId() {
 		return StoreId;
 	}
+	/**
+	 * store id setter
+	 * @param storeId- int
+	 */
 	public void setStoreId(int storeId) {
 		StoreId = storeId;
 	}
+	
+	/**
+	 * store address getter
+	 * @return String- store address
+	 */
 	public String getStore_Address() {
 		return Store_Address;
 	}
+	
+	/**
+	 * store address setter
+	 * @param store_Address- String
+	 */
 	public void setStore_Address(String store_Address) {
 		Store_Address = store_Address;
 	}
 	
+	/**
+	 * store amount of orders getter
+	 * @return int- amount
+	 */
 	public int getQuantityOfOrders() {
 		return QuantityOfOrders;
 	}
 
+	/**
+	 * store amount of orders setter
+	 * @param quantityOfOrders- int
+	 */
 	public void setQuantityOfOrders(int quantityOfOrders) {
 		QuantityOfOrders = quantityOfOrders;
 	}
+	
+	/**
+	 * to string override method
+	 */
 	@Override
 	public String toString() {
 		return "Store [StoreId=" + StoreId + ", Store_Address=" + Store_Address + ", QuantityOfOrders="
